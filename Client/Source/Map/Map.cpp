a6a21d6a5590568758b835539ddb74e601e4b629
LouisLanganay <louislanganay@gmail.com>
2024-05-25 13:34:07 +0200

    feat(feat/Graphical): Add to update map size and tiles
     the. This commit bumping version.

--- Client/Source/Map/Map.cpp
+++ Client/Source/Map/Map.cpp
@@ -9,15 +9,78 @@
 
 using namespace Zappy;
 
-Map::Map() : mapVertices(sf::Quads, 100)
+Map::Map()
+{
+    setSize(0, 0);
+}
+
+Map::~Map()
 {
 }
 
-void Map::update(const std::string &data)
+void Map::setSize(int width, int height)
 {
+    this->width = width;
+    this->height = height;
+    tiles.resize(height, std::vector<Tile>(width));
+    updateDrawable();
 }
 
-const sf::Drawable& Map::getDrawable() const
+void Map::updateTile(int x, int y, const std::vector<int>& resources)
 {
-    return mapVertices;
+    if (x >= 0 && x < width && y >= 0 && y < height && resources.size() == 6) {
+        tiles[y][x].linemate.setQuantity(resources[0]);
+        tiles[y][x].deraumere.setQuantity(resources[1]);
+        tiles[y][x].sibur.setQuantity(resources[2]);
+        tiles[y][x].mendiane.setQuantity(resources[3]);
+        tiles[y][x].phiras.setQuantity(resources[4]);
+        tiles[y][x].thystame.setQuantity(resources[5]);
+        updateDrawable();
+    }
+}
+
+void Map::addPlayer(std::unique_ptr<Player> player)
+{
+    int playerNumber = player->getPlayerNumber();
+    players[playerNumber] = std::move(player);
+}
+
+void Map::removePlayer(int playerNumber)
+{
+    players.erase(playerNumber);
+}
+
+Player* Map::getPlayer(int playerNumber)
+{
+    auto it = players.find(playerNumber);
+    if (it != players.end())
+        return it->second.get();
+    return nullptr;
+}
+
+void Map::updateDrawable()
+{
+    mapDrawable.clear();
+    mapDrawable.setPrimitiveType(sf::Quads);
+    mapDrawable.resize(width * height * 4);
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            const Tile& tile = tiles[y][x];
+            sf::Vertex* quad = &mapDrawable[(x + y * width) * 4];
+            quad[0].position = sf::Vector2f(x * 32, y * 32);
+            quad[1].position = sf::Vector2f((x + 1) * 32, y * 32);
+            quad[2].position = sf::Vector2f((x + 1) * 32, (y + 1) * 32);
+            quad[3].position = sf::Vector2f(x * 32, (y + 1) * 32);
+            quad[0].color = sf::Color::White;
+            quad[1].color = sf::Color::White;
+            quad[2].color = sf::Color::White;
+            quad[3].color = sf::Color::White;
+        }
+    }
+
+}
+
+sf::Drawable& Map::getDrawable()
+{
+    return mapDrawable;
 }
--- Client/Source/Map/Map.hpp
+++ Client/Source/Map/Map.hpp
@@ -9,18 +9,49 @@
 #define MAP_HPP_
 
 #include <vector>
+#include <unordered_map>
+#include <memory>
 #include <SFML/Graphics.hpp>
+#include "IResources.hpp"
+#include "Player/Player.hpp"
+#include "Resources/IResources.hpp"
+#include "Resources/Linemate/Linemate.hpp"
+#include "Resources/Deraumere/Deraumere.hpp"
+#include "Resources/Sibur/Sibur.hpp"
+#include "Resources/Mendiane/Mendiane.hpp"
+#include "Resources/Phiras/Phiras.hpp"
+#include "Resources/Thystame/Thystame.hpp"
+#include "Resources/Food/Food.hpp"
 
 namespace Zappy {
     class Map {
         public:
             Map();
-            void update(const std::string& data);
-            const sf::Drawable& getDrawable() const;
+            ~Map();
+            void setSize(int width, int height);
+            void updateTile(int x, int y, const std::vector<int>& resources);
+            void addPlayer(std::unique_ptr<Player> player);
+            void removePlayer(int playerNumber);
+            Player* getPlayer(int playerNumber);
+            sf::Drawable& getDrawable();
 
         private:
-            sf::VertexArray mapVertices;
-    };
+            int width;
+            int height;
+            struct Tile {
+                Resources::Linemate linemate;
+                Resources::Deraumere deraumere;
+                Resources::Sibur sibur;
+                Resources::Mendiane mendiane;
+                Resources::Phiras phiras;
+                Resources::Thystame thystame;
+                Resources::Food food;
+            };
+            std::vector<std::vector<Tile>> tiles;
+            std::unordered_map<int, std::unique_ptr<Player>> players;
+            sf::VertexArray mapDrawable;
+            void updateDrawable();
+        };
 };
 
 #endif /* !MAP_HPP_ */
