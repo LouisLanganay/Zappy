85e765b6d1d8c90cbe66b9434f0df01daf78a184
blaizot <guillaume.blaizot@epitech.eu>
2024-06-04 10:03:56 +0200

    feat(gui): implement pic command for GUI

---
+++ Server/src/gui/pic.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy
+** File description:
+** pic
+*/
+
+#include "server/gui.h"
+
+void pic(
+    const zappy_server_t *server,
+    const int interlocutor,
+    UNUSED const char *message)
+{
+
+}
