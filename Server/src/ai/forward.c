227ac194b535969810659371d0aedc1fc31c0bb0
mlargeot <morgan.largeot@epitech.eu>
2024-06-05 17:04:08 +0200

    feat(forward): Add forward command content

--- Server/src/ai/forward.c
+++ Server/src/ai/forward.c
@@ -10,8 +10,19 @@
 #include "server/ai_header.h"
 
 void forward(
-    UNUSED const zappy_server_t *server,
-    UNUSED ai_t *ai,
+    const zappy_server_t *server,
+    ai_t *ai,
     UNUSED const char *message)
 {
+    switch(ai->orientation) {
+        case(NORTH):
+            ai->pos.y = (ai->pos.y + server->height - 1) % server->height;
+        case(WEST):
+            ai->pos.x = (ai->pos.x + server->width - 1) % server->width;
+        case(EAST):
+            ai->pos.x = (ai->pos.x + 1) % server->width;
+        case(SOUTH):
+            ai->pos.y = (ai->pos.y + 1) % server->height;
+    }
+    protocol_server_send_message(server->socket, ai->fd, "ok\n");
 }
