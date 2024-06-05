85769306ca7a32d99360e80d102b87c25cc8af68
mlargeot <morgan.largeot@epitech.eu>
2024-06-05 17:06:58 +0200

    refactor(forward): Fix missing break statements

--- Server/src/ai/forward.c
+++ Server/src/ai/forward.c
@@ -17,12 +17,16 @@ void forward(
     switch(ai->orientation) {
         case(NORTH):
             ai->pos.y = (ai->pos.y + server->height - 1) % server->height;
+            break;
         case(WEST):
             ai->pos.x = (ai->pos.x + server->width - 1) % server->width;
+            break;
         case(EAST):
             ai->pos.x = (ai->pos.x + 1) % server->width;
+            break;
         case(SOUTH):
             ai->pos.y = (ai->pos.y + 1) % server->height;
+            break;
     }
     protocol_server_send_message(server->socket, ai->fd, "ok\n");
 }
