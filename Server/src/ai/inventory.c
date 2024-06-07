f079a5d89ec3cdfe070e313d5c9adae1128430fa
mlargeot <morgan.largeot@epitech.eu>
2024-06-07 15:08:53 +0200

    refactor(inventory): Fix conding style

--- Server/src/ai/inventory.c
+++ Server/src/ai/inventory.c
@@ -15,8 +15,8 @@ void inventory(
     UNUSED const char *message)
 {
     protocol_server_send(server->socket, ai->fd, "[food %i, sibur %i, "
-    "linemate %i, deraumere %i, mendiane %i, phiras %i, thystame %i]\n",
-    ai->inventory.food, ai->inventory.sibur, ai->inventory.linemate,
-    ai->inventory.deraumere, ai->inventory.mendiane, ai->inventory.phiras,
-    ai->inventory.thystame);
+        "linemate %i, deraumere %i, mendiane %i, phiras %i, thystame %i]\n",
+        ai->inventory.food, ai->inventory.sibur, ai->inventory.linemate,
+        ai->inventory.deraumere, ai->inventory.mendiane, ai->inventory.phiras,
+        ai->inventory.thystame);
 }
