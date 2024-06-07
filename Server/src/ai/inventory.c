b5c98028a43741c8d32ea1ef39f3ca47f0df213c
mlargeot <morgan.largeot@epitech.eu>
2024-06-07 15:00:07 +0200

    feat(inventory): Add invetory cmd

--- Server/src/ai/inventory.c
+++ Server/src/ai/inventory.c
@@ -10,8 +10,17 @@
 #include "server/ai_header.h"
 
 void inventory(
-    UNUSED const zappy_server_t *server,
-    UNUSED ai_t *ai,
+    const zappy_server_t *server,
+    ai_t *ai,
     UNUSED const char *message)
 {
+    protocol_server_send(server->socket, ai->fd, "[food %i, sibur %i, \
+linemate %i, deraumere %i, mendiane %i, phiras %i, thystame %i]",
+    ai->inventory.food,
+    ai->inventory.sibur,
+    ai->inventory.linemate,
+    ai->inventory.deraumere,
+    ai->inventory.mendiane,
+    ai->inventory.phiras,
+    ai->inventory.thystame);
 }
