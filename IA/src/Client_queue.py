bb1dfab4dfb165f1ca01d6f72308b89153fb55af
Valentin Peron <valentin.peron@epitech.eu>
2024-06-16 10:33:24 +0200

    feat(Client_queue.py): Refactor Client_queue.py to use ParseArgs class for argument parsing

--- IA/src/Client_queue.py
+++ IA/src/Client_queue.py
@@ -2,69 +2,7 @@
 import socket
 import sys
 import selectors
-import random
-import time
-
-class ParseArgs:
-    def __init__(self):
-        self.host = 'localhost'
-        self.port = -1
-        self.name = ''
-
-    def check_type(self):
-        try:
-            self.port = int(self.port)
-        except ValueError:
-            self.print_invalid_argument()
-            sys.exit(84)
-        
-        if self.port == -1 or self.name == '':
-            self.print_usage()
-            sys.exit(84)
-
-        self.check_host()
-        self.check_port()
-        self.check_name()
-
-    def check_host(self):
-        try:
-            socket.gethostbyname(self.host)
-        except socket.gaierror:
-            print("Error: Invalid host")
-            sys.exit(84)
-
-    def check_port(self):
-        if self.port < 0 or self.port > 65535:
-            print("Error: Port out of range")
-            sys.exit(84)
-
-    def check_name(self):
-        if len(self.name) > 32:
-            print("Error: Name too long")
-            sys.exit(84)
-
-    def parse(self, args):
-        for i in range(0, len(args), 2):
-            if args[i] == '-p':
-                self.port = args[i + 1]
-            elif args[i] == '-n':
-                self.name = args[i + 1]
-            elif args[i] == '-h':
-                self.host = args[i + 1]
-            else:
-                self.print_invalid_argument()
-                sys.exit(84)
-
-        self.check_type()
-        return self.host, self.port, self.name
-
-    def print_usage(self):
-        print("USAGE: ./zappy_ai -p port -n name -h machine")
-        sys.exit(84)
-
-    def print_invalid_argument(self):
-        print("Invalid argument")
-        sys.exit(84)
+from ParseArgs import ParseArgs
 
 class Client:
     def __init__(self, host, port, name):
