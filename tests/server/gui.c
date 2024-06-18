81099174bd919f2034ff06fa16a48ca32f000f8a
Neo-Diamons <romain.hure@epitech.eu>
2024-06-18 17:12:42 +0200

    test(test/server): add test for gui file.

---
+++ tests/server/gui.c
@@ -0,0 +1,22 @@
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+#include "server.h"
+
+Test(gui_send_to_all, check_if_message_is_sent_to_all, .init = cr_redirect_stdout)
+{
+    zappy_server_t server = {};
+    gui_t gui1 = {.fd = 1};
+    gui_t gui2 = {.fd = 2};
+    gui_t gui3 = {.fd = 3};
+
+    TAILQ_INIT(&server.guis);
+    TAILQ_INSERT_TAIL(&server.guis, &gui1, entries);
+    TAILQ_INSERT_TAIL(&server.guis, &gui2, entries);
+    TAILQ_INSERT_TAIL(&server.guis, &gui3, entries);
+    gui_send_to_all(&server, "Hello World!");
+    cr_assert_stdout_eq_str(
+        "To client 1: Hello World!\n"
+        "To client 2: Hello World!\n"
+        "To client 3: Hello World!\n");
+}
