5321f4b91432c6665e844ffb7660776d57c61cf5
blaizot <guillaume.blaizot@epitech.eu>
2024-06-21 00:47:07 +0200

    feat(game): add initialization for cmd to handle action time && set player_life to 126

--- Server/src/server.c
+++ Server/src/server.c
@@ -7,16 +7,64 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
-#include "server.h"
-#include "server/game.h"
-
 #include <string.h>
+
+#include "server/game.h"
+#include "server.h"
+
+
+static void fill_cmds(zappy_server_t *server)
+{
+    server->cmd[6]->active = false;
+    server->cmd[6]->cmd = "Connect_nbr";
+    server->cmd[6]->action_time = 0;
+    server->cmd[7]->active = false;
+    server->cmd[7]->cmd = "Fork";
+    server->cmd[7]->action_time = 42;
+    server->cmd[8]->active = false;
+    server->cmd[8]->cmd = "Eject";
+    server->cmd[8]->action_time = 7;
+    server->cmd[9]->active = false;
+    server->cmd[9]->cmd = "Take";
+    server->cmd[9]->action_time = 7;
+    server->cmd[10]->active = false;
+    server->cmd[10]->cmd = "Set";
+    server->cmd[10]->action_time = 7;
+    server->cmd[11]->active = false;
+    server->cmd[11]->cmd = "Incantation";
+    server->cmd[11]->action_time = 300;
+}
+
+static void fill_cmd(zappy_server_t *server)
+{
+    server->cmd = malloc(sizeof(cmd_t *) * 12);
+    server->cmd[0]->active = false;
+    server->cmd[0]->cmd = "Forward";
+    server->cmd[0]->action_time = 7;
+    server->cmd[1]->active = false;
+    server->cmd[1]->cmd = "Right";
+    server->cmd[1]->action_time = 7;
+    server->cmd[2]->active = false;
+    server->cmd[2]->cmd = "Left";
+    server->cmd[2]->action_time = 7;
+    server->cmd[3]->active = false;
+    server->cmd[3]->cmd = "Look";
+    server->cmd[3]->action_time = 7;
+    server->cmd[4]->active = false;
+    server->cmd[4]->cmd = "Inventory";
+    server->cmd[4]->action_time = 1;
+    server->cmd[5]->active = false;
+    server->cmd[5]->cmd = "Broadcast";
+    server->cmd[5]->action_time = 7;
+    fill_cmds(server);
+}
 
 void server_create(
     zappy_server_t *server)
 {
     *server = (zappy_server_t){ .port = 4242, .width = 10, .height = 10 };
+    server->last_update = (struct timespec){ .tv_sec = 0, .tv_nsec = 0 };
+    fill_cmd(server);
     TAILQ_INIT(&server->ais);
     TAILQ_INIT(&server->teams);
     TAILQ_INIT(&server->guis);
@@ -75,6 +123,8 @@ static bool display_server(
 
 bool zappy_server(zappy_server_t *server)
 {
+    ai_t *ai;
+
     if (!server || server->port < 1024
         || server->width <= 0 || server->height <= 0
         || server->clients_nb <= 0 || server->freq <= 0
@@ -82,6 +132,8 @@ bool zappy_server(zappy_server_t *server)
         return false;
     display_server(server);
     trigger_meteor(server);
+    TAILQ_FOREACH(ai, &server->ais, entries)
+        ai->player_life = 126;
     while (protocol_server_is_open()) {
         game_update(server);
         protocol_server_listen(server->socket);
