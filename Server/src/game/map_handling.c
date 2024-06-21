e651e10ac7ecefcfee75703d81ba9eb1bb534a4a
blaizot <guillaume.blaizot@epitech.eu>
2024-06-21 03:03:02 +0200

    feat(game): Refactor fill_map function to use add_ressources helper

--- Server/src/game/map_handling.c
+++ Server/src/game/map_handling.c
@@ -9,10 +9,20 @@
 #include <stdlib.h>
 #include <time.h>
 
-void fill_map(
+static void add_ressources(
+    zappy_server_t *server,
+    uint16_t x,
+    uint16_t y,
+    size_t res)
+{
+    if (res < sizeof(ressources) / sizeof(ressources[0])) {
+        ressources[res].func(server, x, y);
+    }
+}
+
+static void fill_map(
     const size_t num_resources,
     size_t quantities[num_resources],
-    uint16_t *resources[],
     zappy_server_t *server)
 {
     uint16_t x = 0;
@@ -27,7 +37,7 @@ void fill_map(
         for (size_t count = 0; count < quantities[res_index]; ++count) {
             x = rand() % server->width;
             y = rand() % server->height;
-            resources[res_index][y * server->width + x]++;
+            add_ressources(server, x, y, res_index);
         }
     }
 }
@@ -36,19 +46,10 @@ void trigger_meteor(zappy_server_t *server)
 {
     const double densities[] = {0.5, 0.3, 0.15, 0.1, 0.1, 0.08, 0.05};
     const size_t num_resources = sizeof(densities) / sizeof(densities[0]);
-    uint16_t *resources[] = {
-        &server->map[0][0].food,
-        &server->map[0][0].linemate,
-        &server->map[0][0].deraumere,
-        &server->map[0][0].sibur,
-        &server->map[0][0].mendiane,
-        &server->map[0][0].phiras,
-        &server->map[0][0].thystame
-    };
     size_t quantities[num_resources];
     size_t map_area = server->width * server->height;
 
     for (size_t i = 0; i < num_resources; ++i)
         quantities[i] = (size_t)(map_area * densities[i]);
-    fill_map(num_resources, quantities, resources, server);
+    fill_map(num_resources, quantities, server);
 }
