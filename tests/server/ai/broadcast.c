a00f380c30cd20bf856c200ef99c31bf1bf99200
mlargeot <morgan.largeot@epitech.eu>
2024-06-21 15:46:48 +0200

    feat(tests/broadcast): Add broadcast subject test

--- tests/server/ai/broadcast.c
+++ tests/server/ai/broadcast.c
@@ -33,15 +33,15 @@ Test(broadcast, check_message_reception)
     vector2_t sender_pos = {9, 2};
     vector2_t reciever_pos = {2, 5};
     ai_t sender = {.fd = 1, .pos = sender_pos, .orientation = NORTH};
-    ai_t reciever = {.fd = 2, .pos = reciever_pos, .orientation = WEST};
+    ai_t reciever = {.fd = 2, .pos = reciever_pos, .orientation = EAST};
 
     TAILQ_INIT(&server.ais);
     TAILQ_INSERT_TAIL(&server.ais, &sender, entries);
     TAILQ_INSERT_TAIL(&server.ais, &reciever, entries);
-    //cr_redirect_stdout();
+    cr_redirect_stdout();
     broadcast_text(&server, &sender, " Hello World!");
-    //cr_assert_stdout_eq_str("To client 2: message 4, Hello World!\n"
-    //                        "To client 1: ok\n");
+    cr_assert_stdout_eq_str("To client 2: message 4, Hello World!\n"
+                            "To client 1: ok\n");
 }
 
 Test(broadcast, check_message_reception_on_the_same_tile)
