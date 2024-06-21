defd38350a5aaff425f7c5b7070f2d908f9410f9
mlargeot <morgan.largeot@epitech.eu>
2024-06-21 13:30:04 +0200

    refactor(tests/death.c): Fix useless .init

--- tests/server/ai/death.c
+++ tests/server/ai/death.c
@@ -3,7 +3,7 @@
 
 #include "server/ai_header.h"
 
-Test(death, check_if_message_is_sent_to_ai, .init = cr_redirect_stdout)
+Test(death, check_if_message_is_sent_to_ai)
 {
     zappy_server_t server = {};
     ai_t ai1 = {.fd = 1};
@@ -11,5 +11,6 @@ Test(death, check_if_message_is_sent_to_ai, .init = cr_redirect_stdout)
     TAILQ_INIT(&server.ais);
     TAILQ_INSERT_TAIL(&server.ais, &ai1, entries);
     death(&server, &ai1, "");
+    cr_redirect_stdout();
     cr_assert_stdout_eq_str("To client 1: dead\n");
 }
