dad9940d2a8b94d9603a45d6b7dc559383833d3e
Valentin Peron <valentin.peron@epitech.eu>
2024-06-21 19:20:37 +0200

    feat(Test.py): Update assert statements in Test.py

--- IA/src/Test.py
+++ IA/src/Test.py
@@ -9,7 +9,8 @@ argv = ["./zappy_ai", "-p", "3001", "-n", "name", "-h", "localhost"]
 
 parse_args = ParseArgs()
 host, port, name = parse_args.parse(argv[1:])
-assert host == "localhost"
-assert port == 3001
-assert name == "name"
+assert host == "localhost", f"Expected host to be 'localhost', but got '{host}'"
+assert port == 3001, f"Expected port to be 3001, but got {port}"
+assert name == "name", f"Expected name to be 'name', but got '{name}'"
 
+print("Test 1 passed")
