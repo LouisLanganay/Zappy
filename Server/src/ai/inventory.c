9cd7e40478765927dd2ffb774ef6acf516eba036
mlargeot <morgan.largeot@epitech.eu>
2024-06-22 11:56:51 +0200

    refactor(inventory): Remove \n that cause issue

--- Server/src/ai/inventory.c
+++ Server/src/ai/inventory.c
@@ -15,7 +15,7 @@ void inventory(
     UNUSED const char *message)
 {
     protocol_server_send(server->socket, ai->fd, "[food %i, sibur %i, "
-        "linemate %i, deraumere %i, mendiane %i, phiras %i, thystame %i]\n",
+        "linemate %i, deraumere %i, mendiane %i, phiras %i, thystame %i]",
         ai->inventory.food, ai->inventory.sibur, ai->inventory.linemate,
         ai->inventory.deraumere, ai->inventory.mendiane, ai->inventory.phiras,
         ai->inventory.thystame);
