63a85fc26e42378621a966e18f50fcbdf3204f1f
LouisLanganay <louislanganay@gmail.com>
2024-06-22 22:43:34 +0200

    feat(api): reduce sleep time

--- Client/Source/API/Api.cpp
+++ Client/Source/API/Api.cpp
@@ -103,7 +103,7 @@ void Api::fetchDataLoop()
 {
     while (_isRunning && protocol_client_is_connected(_client)) {
         fetchDataFromServer();
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 }
 
