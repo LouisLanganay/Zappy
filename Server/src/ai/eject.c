def122c47770fa4f0169f7bd6727b34e79f8b2b2
Neo-Diamons <romain.hure@epitech.eu>
2024-06-23 14:50:21 +0200

    fix(test/server): eject not working.

--- Server/src/ai/eject.c
+++ Server/src/ai/eject.c
@@ -10,12 +10,6 @@
 #include "server/ai_header.h"
 #include "server/gui.h"
 
-static orientation_t convert_orientation(
-    const orientation_t orientation)
-{
-    return (orientation_t[]){ WEST, SOUTH, EAST, NORTH }[orientation - 1];
-}
-
 static void eject_egg(
     zappy_server_t *server,
     const ai_t *ai,
@@ -34,11 +28,11 @@ static void eject_egg(
     }
 }
 
-//        SOUTH WEST NORTH EAST
-// SOUTH  | 6   | 8  | 2   | 4
-// WEST   | 4   | 6  | 8   | 2
-// NORTH  | 2   | 4  | 6   | 8
-// EAST   | 8   | 2  | 4   | 6
+//        NORTH EAST  SOUTH WEST
+// NORTH  |  5  |  3  |  1  |  7
+// EAST   |  7  |  5  |  3  |  1
+// SOUTH  |  1  |  7  |  5  |  3
+// WEST   |  3  |  1  |  7  |  5
 void eject(
     zappy_server_t *server,
     ai_t *ai,
@@ -47,15 +41,20 @@ void eject(
     ai_t *target;
     bool ejected = false;
 
-    TAILQ_FOREACH(target, &server->ais, entries)
-        if (target->fd != ai->fd
-            && target->pos.x == ai->pos.x && target->pos.y == ai->pos.y) {
-            server_send(server, target->fd, "eject: %i",
-                (uint8_t[]){ 8, 6, 4, 2 }[(convert_orientation(WEST)
-                    + convert_orientation(NORTH) - 1) % 4]);
-            ejected = true;
-            pex(server, target);
-        }
+    TAILQ_FOREACH(target, &server->ais, entries) {
+        if (target->fd == ai->fd
+            || target->pos.x != ai->pos.x || target->pos.y != ai->pos.y)
+            continue;
+        server_send(server, target->fd, "eject: %i",
+            (uint8_t[4][4]){
+                { 5, 3, 1, 7 },
+                { 7, 5, 3, 1 },
+                { 1, 7, 5, 3 },
+                { 3, 1, 7, 5 }
+            }[target->orientation - 1][ai->orientation - 1]);
+        ejected = true;
+        pex(server, target);
+    }
     eject_egg(server, ai, &ejected);
     if (ejected)
         server_send(server, ai->fd, "ok");
