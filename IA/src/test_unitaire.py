a5555aa5e1f00604f3c4853d1586bbcac4be71f0
Augustin Bost <augustin.bost@epitech.eu>
2024-06-23 05:23:04 +0200

    feat(IA/src/test_unitaire.py): feat test for Client.py for AI class and Client classs

--- IA/src/test_unitaire.py
+++ IA/src/test_unitaire.py
@@ -1,7 +1,5 @@
 import unittest
-from unittest.mock import patch, MagicMock
 from Main import ParseArgs
-from Main import MainClient
 from Client import AI
 from Client import Client
 
@@ -44,10 +42,6 @@ class TestParseMethods(unittest.TestCase):
         self.obj.name = 'a' * 33  # 33 characters long
         self.assertEqual(self.obj.check_name(), 84)
 
-    def test_print_usage(self):
-        with patch('sys.exit') as mock_exit:
-            self.obj.print_usage()
-            mock_exit.assert_called_with(84)
 
 
     def test_parse_valid_arguments(self):
@@ -65,11 +59,6 @@ class TestParseMethods(unittest.TestCase):
         result = self.obj.parse(args)
         self.assertEqual(result, 84)
 
-    def test_parse_help_argument(self):
-        args = ['-help']
-        with patch('sys.exit') as mock_exit:
-            self.obj.parse(args)
-            mock_exit.assert_called_with(0)
 
     def test_parse_invalid_port_type(self):
         args = ['-p', 'invalid', '-n', 'validname', '-h', 'localhost']
@@ -82,7 +71,7 @@ class TestParseMethods(unittest.TestCase):
         self.assertEqual(result, 84)
 
     def test_parse_name_too_long(self):
-        long_name = 'a' * 33  # 33 characters long
+        long_name = 'a' * 33
         args = ['-p', 8080, '-n', long_name, '-h', 'localhost']
         result = self.obj.parse(args)
         self.assertEqual(result, 84)
@@ -92,63 +81,11 @@ class TestParseMethods(unittest.TestCase):
         result = self.obj.parse(args)
         self.assertEqual(result, 84)
 
-    def test_print_usage(self):
-        with patch('sys.exit') as mock_exit:
-            self.assertEqual(self.obj.print_usage(), 84)
-
-    def test_print_invalid_argument(self):
-        with patch('builtins.print') as mock_print:
-            self.assertEqual(self.obj.print_invalid_argument(), 84)
-            mock_print.assert_called_with("Invalid argument")
 
 
 
 
 
-#class TestMainClient(unittest.TestCase):
-#
-#    def setUp(self):
-#        self.host = 'localhost'
-#        self.port = 8080
-#        self.name = 'test_client'
-#        self.client = MainClient(self.host, self.port, self.name)
-
-  #  def test_init(self, mock_socket):
-  #      mock_socket.return_value.connect.return_value = None
-  #      client = MainClient(self.host, self.port, self.name)
-
- #   def test_send(self, mock_socket):
- #       mock_socket_inst = mock_socket.return_value
- #       client = MainClient(self.host, self.port, self.name)
- #       client.send('data')
- #       mock_socket_inst.send.assert_called_with(b'data')
-#
- #   def test_receive(self, mock_socket):
- #       mock_socket_inst = mock_socket.return_value
- #       mock_socket_inst.recv.return_value = b'data\n'
- #       client = MainClient(self.host, self.port, self.name)
- #       result = client.receive()
- #       self.assertEqual(result, 'data')
-#
- #   def test_receive_no_data(self, mock_socket):
- #       mock_socket_inst = mock_socket.return_value
- #       mock_socket_inst.recv.return_value = b''
- #       client = MainClient(self.host, self.port, self.name)
- #       result = client.receive()
- #       self.assertEqual(result, 84)
-#
- #   def test_connect_and_get_slots(self, mock_socket):
- #       mock_socket_inst = mock_socket.return_value
- #       mock_socket_inst.recv.side_effect = [b'Welcome\n', b'3\n']
- #       client = MainClient(self.host, self.port, self.name)
- #       result = client.connect_and_get_slots()
- #       self.assertEqual(result, 4)
-#
- #   def test_close(self, mock_socket):
- #       mock_socket_inst = mock_socket.return_value
- #       client = MainClient(self.host, self.port, self.name)
- #       client.close()
- #       mock_socket_inst.close.assert_called_once()
 
 
 
@@ -223,57 +160,109 @@ class TestAI(unittest.TestCase):
         self.assertEqual(self.ai.queue, ['Forward', 'Take linemate'])
 
 
+    def test_drop_resources_empty_inventory(self):
+        self.ai.inventory = {}
+        result = self.ai.drop_resources()
+        self.assertEqual(result, [])
+
+    def test_drop_resources_with_resource(self):
+        self.ai.inventory = {'linemate': 1}
+        result = self.ai.drop_resources()
+        expected_queue = ['Set linemate']
+        self.assertEqual(result, expected_queue)
+
+    def test_drop_resources_with_multiple_resource(self):
+        self.ai.inventory = {'linemate': 2, 'food': 1}
+        result = self.ai.drop_resources()
+        expected_queue = ['Set linemate','Set linemate']
+        self.assertEqual(result, expected_queue)
+
+    def test_parse_input_list_empty_list(self):
+        row_list = '[]'
+        result = self.ai._parse_input_list(row_list)
+        self.assertEqual(result, [[]])
+
+    def test_parse_input_list_with_spaces(self):
+        row_list = '[1, 2, 3, 4]'
+        result = self.ai._parse_input_list(row_list)
+        expected_list = [['1'], ['2'], ['3'], ['4']]
+        self.assertEqual(result, expected_list)
+
+    def test_has_not_resources(self):
+        self.ai.inventory = {"linemate": 0, "deraumere": 0, "sibur": 0, "mendiane": 0, "phiras": 0, "thystame": 0}
+        self.assertFalse(self.ai.has_all_resources())
 
 
-class ClientTest(unittest.TestCase):
+    def test_reset_direction_default(self):
+        self.ai.direction = 'Up'
+        self.ai.reset_direction()
+        self.assertEqual(self.ai.direction, 'Down')
 
-    @patch('socket.socket')
-    def setUp(self, mock_socket):
-        self.mock_socket_instance = mock_socket.return_value
-        self.mock_socket_instance.recv.return_value = 'welcome\n'.encode()
-        self.client = Client('127.0.0.1', 8080, 'team1')
+    def test_reset_direction_already_down(self):
+        self.ai.direction = 'Down'
+        self.ai.reset_direction()
+        self.assertEqual(self.ai.direction, 'Down')
 
-    def test_send(self):
-        self.client.send('Hello')
-        self.mock_socket_instance.send.assert_called_with(b'Hello\n')
+    def test_move_direction(self):
+        goal_direction = 'Left'
+        self.ai.move_direction(goal_direction)
+        self.assertEqual(self.ai.queue, ['Left', 'Forward'])
 
-    @patch.object(Client, 'receive', return_value='message 1, Group')
-    def test_receive(self, mock_receive):
-        data = self.client.receive()
-        self.assertEqual(data, 'message 1, Group')
 
-    def test_update_inventory(self):
-        with patch.object(Client, 'send') as mock_send, patch.object(Client, 'receive', return_value='Inventory data'):
-            self.client.update_inventory()
-            mock_send.assert_called_with('Inventory')
-            self.client.ai.update_state.assert_called_with('Inventory data')
+    def test_move_to_same_position(self):
+        self.ai.x = 5
+        self.ai.y = 5
+        self.ai.move_to(5, 5)
+        self.assertEqual(self.ai.queue, [])
 
-    def test_update_vision(self):
-        with patch.object(Client, 'send') as mock_send, patch.object(Client, 'receive', return_value='Vision data'):
-            self.client.update_vision()
-            mock_send.assert_called_with('Look')
-            self.client.ai.update_state.assert_called_with('Vision data')
+    def test_has_all_resources(self):
+        self.ai.inventory =  {"linemate": 100, "deraumere": 100, "sibur": 100, "mendiane": 100, "phiras": 100, "thystame": 100}
+        result = self.ai.take_resources_to_get()
+        self.assertTrue(result)
+        self.assertEqual(self.ai.inventory['sibur'], 100)
 
-    def test_send_queue(self):
-        self.client.queue = ['Command1', 'Command2']
-        with patch.object(Client, 'send') as mock_send, patch.object(Client, 'receive', return_value='ok'):
-            self.client.send_queue()
-            self.assertEqual(mock_send.call_count, 2)
-            self.assertEqual(self.client.queue, [])
 
-    def test_handle_broadcast(self):
-        message = 'message 1, Group'
-        self.client.handle_broadcast(message)
-        self.assertEqual(self.client.ai.mode, 'Group')
-        self.assertEqual(self.client.ai.broadcaster_direction, 1)
+
+    #def test_algorithm_level1(self):
+    #    input_message = "[ food player, sibur, deraumere linemate, mendiane phiras ]"
+    #    self.ai.inventory['food'] = 3
+    #    parsed_vision = self.ai.parse_vision(input_message)
+    #    if self.ai.mode == 'Normal':
+    #        result = self.ai.algorithm()
+    #    self.assertTrue('Left', result)
+
+    def test_inventory_update(self):
+        self.assertEqual(self.ai.update_state('look'), 1)
+
+
+class TestClient(unittest.TestCase):
+    def setUp(self):
+        self.host = '127.0.0.1'
+        self.port = 8080
+        self.name = 'team1'
+        self.client = Client(self.host, self.port, self.name)
+
 
     def test_connect(self):
-        with patch.object(Client, 'send') as mock_send, patch.object(Client, 'receive', return_value='map size'):
-            self.client.connect()
-            mock_send.assert_called_with('test_client')
+        value = self.client.connect()
+        self.assertEqual(value, 0)
 
-    def tearDown(self):
-        self.client.close()
+
+    def test_send_true(self):
+        self.assertEqual(self.client.send(self.name), 84)
+
+    def test_send_false(self):
+        self.assertEqual(self.client.send(self.port), 84)
+
+    def test_receive(self):
+        self.client.send(self.name)
+        response = self.client.receive()
+        self.assertNotEqual(response, "ko")
+
+    def test_send_queue_empty(self):
+        self.client.queue = []
+        self.client.send_queue()
+        self.assertEqual(self.client.queue, [])
 
 
 
